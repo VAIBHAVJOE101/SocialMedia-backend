920e1089b696b1ac3411b8affc84e9163170fd72
VAIBHAVJOE101 <vaibhav251200@gmail.com>
2021-10-11 16:38:35 +0530

    Updated Posts

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
 //middleware
 app.use(express.json());
 app.use(helmet());
-app.use(morgan("common"));
+// app.use(morgan("common"));
 
 app.use("/api/users" , usersRoute);
 app.use("/api/auth" , authRoute);
